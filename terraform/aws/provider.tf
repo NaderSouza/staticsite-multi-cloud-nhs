6af638a1b70bb808feecfa9e77375ee78e9eebae
Selma Hadad <naderhadadsouza@gmail.com>
2023-10-25 20:51:01 -0300

    check

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "staticsite-multicloud-tf-v001"
+    bucket         = "staticsite-multicloud-tf-v001-nadin"
     key            = "terraform.tfstate"
-    dynamodb_table = "staticsite-multicloud-tf-v001"
+    dynamodb_table = "staticsite-multicloud-tf-v001-nadin"
     region         = "us-east-1"
   }
 }
