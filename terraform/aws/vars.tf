b84bac86a1da9549a5b75fc7f1943031208d8916
Selma Hadad <naderhadadsouza@gmail.com>
2023-10-25 21:18:43 -0300

    check

--- terraform/aws/vars.tf
+++ terraform/aws/vars.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
-  default = "staticsite-multicloud-tf-v001-nadin-3"
+  default = "staticsite-multicloud-tf-v001-nadin-5"
 }
 
 variable "website_endpoint" {
--- terraform/azure/vars.tf
+++ terraform/azure/vars.tf
@@ -1,4 +1,4 @@
 variable "storage_account_name" {
     type    = string
-    default = "nhs-3"
+    default = "nhs-5"
 }
